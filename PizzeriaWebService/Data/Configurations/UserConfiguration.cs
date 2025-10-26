572f0fdcce5e96b365799d6ab580bb25cb39d5d7
Rinat <49842585+ReneSung@users.noreply.github.com>
2025-10-26 21:46:01 +0400

    добавлена конфигурация сущности user

---
+++ PizzeriaWebService/Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace PizzeriaWebService.Data.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<UserEntity>
+{
+  public void Configure(EntityTypeBuilder<UserEntity> builder)
+  {
+    builder.ToTable("users");
+    
+    builder.HasKey(u => u.Id);
+    
+    builder.Property(u => u.Id)
+      .HasColumnName("id");
+    
+    builder.Property(u => u.PhoneNumber)
+      .HasColumnName("phone_number");
+
+    builder.HasIndex(u => u.PhoneNumber);
+
+    builder.Property(u => u.PasswordHash)
+      .HasColumnName("password_hash");
+    
+    builder.Property(u => u.Salt)
+      .HasColumnName("salt");
+    
+  }
+}
