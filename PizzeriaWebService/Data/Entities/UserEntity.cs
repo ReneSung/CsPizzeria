e597f88e9f6b52d8d5982c8f684e1a7c8c8fc23e
Rinat <49842585+ReneSung@users.noreply.github.com>
2025-10-26 21:22:29 +0400

    Добавлен класс UserEntity

---
+++ PizzeriaWebService/Data/Entities/UserEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace PizzeriaWebService.Data;
+
+public class UserEntity
+{
+  private Guid Id { get; set; }
+  
+  private string? PhoneNumber { get; set; }
+  
+  private string? PasswordHash { get; set; }
+  
+  private string? Salt { get; set; }
+}
